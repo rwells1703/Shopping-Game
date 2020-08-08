2362add3c72166e2d6b85c980599ef512411cdf4
Richard Wells <rwells1703@gmail.com>
2020-08-08 18:50:35 +0100

    Made player sprite rotate with physics body.

--- core/src/check/out/game/maingame/artists/PlayerDrawer.java
+++ core/src/check/out/game/maingame/artists/PlayerDrawer.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import fernebon.core.base.Nebula;
 import fernebon.core.base.artist.Artist;
 import fernebon.core.base.fermion.FermionList;
@@ -37,8 +38,9 @@ public class PlayerDrawer extends LifeCycleImplementation implements Artist {
     batch.begin();
 
     Shopper player = ((NebulaShop) nebula).player.getPointeeCast();
-    batch.draw(playerTexture, player.getBody().getPosition().x-0.5f, player.getBody().getPosition().y-0.5f, 1f, 1.25f);
-
+    float angle = (float) ((float) player.getBody().getAngle()*180/Math.PI);
+    System.out.println(angle);
+    batch.draw(new TextureRegion(playerTexture), player.getBody().getPosition().x-0.5f, player.getBody().getPosition().y-0.5f, 0.5f, 0.5f, 1f, 1.25f, 1f, 1f, angle);
 
 //    for(Projectile projectile: nebula.fermions().<Projectile>particles(ConstShop.FB_PROJECTILE)){
 ////      batch.draw(playerTexture, )
