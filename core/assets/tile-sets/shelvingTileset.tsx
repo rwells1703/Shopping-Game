09fe0367da7cdc33cec3e9b6e6b9db3d112ff3b8
Richard Wells <rwells1703@gmail.com>
2020-08-11 01:38:32 +0100

    Improved tileset textures, map, flooring.

--- core/assets/tile-sets/shelvingTileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="shelvingTileset" tilewidth="128" tileheight="128" tilecount="2" columns="2">
- <image source="shelvingTileset.png" width="256" height="128"/>
-</tileset>
---
+++ core/assets/tile-sets/tileset.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="tileset" tilewidth="128" tileheight="128" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="shelves/shelf_dry_edge_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="shelves/shelf_dry_edge_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="shelves/shelf_dry_edge_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="shelves/shelf_dry_edge_4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="shelves/shelf_dry_full_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="shelves/shelf_dry_full_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="shelves/shelf_dry_full_3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="shelves/shelf_dry_full_4.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="shelves/shelf_dry_seperate_1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="shelves/shelf_dry_seperate_2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="shelves/shelf_dry_seperate_3.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="shelves/shelf_dry_seperate_4.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="floor.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="shelves/shelf_dry_corner.png"/>
+ </tile>
+</tileset>
