3c418fa3763c6081e9f61bb3f5d70becb2b4bb0e
Fraser Masson <frasermasson77@gmail.com>
2020-08-21 15:38:17 +0100

    Projectile shoots in direction of mouse, not direction of trolley

--- core/src/check/out/game/maingame/effects/LaunchProjectile.java
+++ core/src/check/out/game/maingame/effects/LaunchProjectile.java
@@ -3,16 +3,21 @@ package check.out.game.maingame.effects;
 import check.out.game.maingame.ConstShop;
 import check.out.game.maingame.fermions.Player;
 import check.out.game.maingame.fermions.Projectile;
+import check.out.game.maingame.nonfermions.InputCore;
 import check.out.game.maingame.stellar.NebulaShop;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import fernebon.core.base.Nebula;
 import fernebon.core.base.effect.Effect;
 import fernebon.core.base.fermion.FermionList;
 import fernebon.core.util.LifeCycleImplementation;
 
 public class LaunchProjectile extends LifeCycleImplementation implements Effect {
+    private final float INITIALPROJECTILEVELOCITY = 20;
+    private final float PROJECTILEOFFSET = 0.7F;
+
     @Override
     public int getPriority() {
         return ConstShop.EP_LAUNCH_PROJECTILE;
@@ -37,19 +42,22 @@ public class LaunchProjectile extends LifeCycleImplementation implements Effect 
 
 
         //TODO scrolling not working properly - might be my scroll wheel is dodgy?
-//        Gdx.input.setInputProcessor(new InputCore());
-//        if(Gdx.input.getInputProcessor().scrolled(1)){
-////            System.out.println("down");
-//        }else if(Gdx.input.getInputProcessor().scrolled(-1)){
-//            System.out.println("up");
-//        }
+        /*Gdx.input.setInputProcessor(new InputCore());
+        if(Gdx.input.getInputProcessor().scrolled(1)){
+            System.out.println("down");
+        }else if(Gdx.input.getInputProcessor().scrolled(-1)){
+            System.out.println("up");
+        }*/
 
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) || Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
             try {
                 player.removeOneOf(Projectile.SELECTED_TYPE);
 
-                Vector2 projectilePos = new Vector2(playerPos.x - (float) (1 * Math.sin(playerAngle)), playerPos.y + (float) (1 * Math.cos(playerAngle)));
-                Vector2 projectileVel = new Vector2(5 * (float) Math.cos(playerAngle + Math.PI / 2), 5 * (float) Math.sin(playerAngle + Math.PI / 2));
+                Vector3 mousePos = ((NebulaShop)nebula).mainCamera.unproject(new Vector3(Gdx.input.getX(0),Gdx.input.getY(0),0));
+                Vector2 projectileVel = new Vector2(mousePos.x-playerPos.x,mousePos.y-playerPos.y);
+                projectileVel.setLength2(INITIALPROJECTILEVELOCITY);
+                Vector2 projectilePos = playerPos.add(new Vector2(projectileVel).setLength2(PROJECTILEOFFSET));
+                projectileVel.add(player.getBody().getLinearVelocity());
                 Projectile projectile = new Projectile(((NebulaShop) nebula).world(), projectilePos, projectileVel, Projectile.SELECTED_TYPE);
                 list.add(() -> projectile);
 
